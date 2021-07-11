c6688278cdd5d20b49a17dd7e49e4b19fb751073
Ankit Prasad <ankit28prasad9@gmail.com>
2021-07-11 10:58:05 +0530

    Update recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -6,8 +6,8 @@ class recom():
 
     def recom_info(self):
         self.driver.get(url='https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm')
-        select = self.driver.find_element_by_xpath('//*[@id="lister-sort-by-options"]')
-        select.click()
+        select1 = self.driver.find_element_by_xpath('//*[@id="lister-sort-by-options"]/option[2]')
+        select1.click()
 
 bot = recom()
 bot.recom_info()
