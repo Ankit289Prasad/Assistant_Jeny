82cd69e552fda3e3eb22c72fa18914ae62cfcdfc
Ankit Prasad <ankit28prasad9@gmail.com>
2021-07-11 13:37:37 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -8,5 +8,19 @@ from words import *
 
 r1 = sr.Recognizer()
 engine = p.init()
-engine.say("Hello buddy!! I am Jeny, how may I help you")
+engine.say("Hello buddy!! I am Jeny, how may I help you?")
+print("Jeny : Hello buddy!! I am Jeny, how may I help you?")
 engine.runAndWait()
+with sr.Microphone() as source:
+    audio = r1.listen(source)
+    try:
+        text = r1.recognize_google(audio)
+        print("You  : "+text)
+    except sr.UnknownValueError:
+        engine.say("Unable to Understand!")
+        print("Jeny : Unable to Understand!")
+        engine.runAndWait()
+    except sr.RequestError as e:
+        engine.say(e)
+        print(e)
+        engine.runAndWait()
